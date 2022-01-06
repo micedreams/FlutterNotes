626095abe9de56200d791912e50104a2f0dbb291
aku <micedreams@gmail.com>
2022-01-06 23:34:45 +0530

    BLoC pattern test 1

---
+++ random1/android/app/src/main/kotlin/com/example/random1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.random1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ two_sum.dart
@@ -0,0 +1,26 @@
+ void main() { 
+  var nums =[2,11,7,15];
+  var target =9;
+  var answer = twoSum(nums,target);
+  
+  print(answer);
+   
+}
+
+twoSum(nums,target){
+var i =0;
+var element =  nums[i];
+var list = nums;
+  
+  print(i);
+  print(element);
+  list.remove(element); 
+  print(list);
+   
+  if (list1.any((item) => list2.contains(item))) {
+    // Lists have at least one common element
+} else {
+    // Lists DON'T have any common element
+}
+}
+
